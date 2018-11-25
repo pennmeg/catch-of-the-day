c5c76a58b517105096ab5ce5e48394b0ccffa1fa
Megan Penn <penn.meg@gmail.com>
2018-11-25 09:55:12 -0500

    events and routing

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = (event) => {
+    // Stop form from submitting
+    event.preventDefault();
+    // Get text from input
+    console.log(this.myInput.value.value);
+    // Change page to /store/store-name
+    this.props.history.push(`/store/fish-store`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={"fish-store"}/>
         <button type="submit">Visit Store</button>
       </form>
     )
