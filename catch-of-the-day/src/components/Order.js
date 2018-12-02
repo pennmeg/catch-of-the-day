778f218765b30b263293a15bdf6363b9d8ff285e
Megan Penn <penn.meg@gmail.com>
2018-12-02 18:57:36 -0500

    added styles for count

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -21,9 +21,16 @@ class Order extends React.Component {
     return (
       <CSSTransition classNames="order" key={key} timeout={{ enter: 500, exit: 500 }}>
         <li key={key}>
-          {count} lbs {fish.name}
-          {formatPrice(count * fish.price)}
-          <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+          <span>
+            <TransitionGroup component="span" className="count">
+              <CSSTransition classNames="count" key={count} timeout={{ enter: 500, exit: 500 }}>
+                <span>{count}</span>
+              </CSSTransition>
+            </TransitionGroup>
+            lbs {fish.name}
+            {formatPrice(count * fish.price)}
+            <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+          </span>
         </li>
       </CSSTransition>
     );
