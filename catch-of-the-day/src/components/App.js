845ad8a78ce146af5c21683055845619c2536dba
Megan Penn <penn.meg@gmail.com>
2018-12-02 15:36:33 -0500

    local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,12 +12,22 @@ class App extends React.Component {
     order: {},
   };
   componentDidMount() {
+    const { params } = this.props.match;
+    // first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
     this.ref = base.syncState(`${this.props.match.params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
   };
-  componentWillUmount(){
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
+  };
+  componentWillUmount() {
     base.removeBinding(this.ref);
   };
   addFish = (fish) => {
